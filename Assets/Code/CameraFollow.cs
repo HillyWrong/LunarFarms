3ebdeea5de0f8eb00ea9243246421a247312f809
HillyWrong <hollyacwright06@gmail.com>
2026-03-04 11:51:12 +0000

    fixed layer order and added sprite sheet

---
+++ Assets/Code/CameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    Vector3 camOffset;
+    void Start()
+    {
+        camOffset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position + camOffset;
+    }
+}
