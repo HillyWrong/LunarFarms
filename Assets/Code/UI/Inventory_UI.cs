955e3c201bdaf95bea9ef1445ce5555ef652ccdf
HillyWrong <hollyacwright06@gmail.com>
2026-03-10 18:47:48 +0000

    Working on Drag and Drop for Inventory items

--- Assets/Code/UI/Inventory_UI.cs
+++ Assets/Code/UI/Inventory_UI.cs
@@ -1,11 +1,25 @@
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using Unity.VisualScripting;
+
 
 public class Inventory_UI : MonoBehaviour
 {
     public GameObject inventoryPanel;
     public Player player;
     public List<Slots_UI> slots = new List<Slots_UI>();
+
+    [SerializeField] private Canvas canvas;
+
+    private Slots_UI draggedSlot;
+    private Image draggedIcon;
+
+    private void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+
+    }
 
 void Start()
     {
@@ -66,5 +80,46 @@ void Start()
         }
         
         
+    }
+
+    public void SlotBeginDrag(Slots_UI slot)
+    {
+        draggedSlot = slot;
+        draggedIcon = Instantiate(draggedSlot.itemIcon);
+        draggedIcon.transform.SetParent(canvas.transform);
+        draggedIcon.raycastTarget = false;
+        draggedIcon.rectTransform.sizeDelta = new Vector2(1, 1);
+
+        MoveToMousePosition(draggedIcon.gameObject);
+        Debug.Log("Start Drag: " + draggedSlot.name);
+    }
+
+    public void SlotDrag()
+    {
+        Debug.Log("Dragging: " + draggedSlot.name);
+    }
+
+    public void SlotEndDrag()
+    {
+        Destroy(draggedIcon.gameObject);
+        draggedIcon = null;
+        Debug.Log("Done Dragging: " + draggedSlot.name);
+    }
+
+    public void SlotDrop(Slots_UI slot)
+    {
+        Debug.Log("Dropped : " + draggedSlot.name + "on : " + slot.name);
+    }
+
+    private void MoveToMousePosition(GameObject toMove)
+    {
+        if(canvas != null)
+        {
+            Vector2 position;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, null, out position);
+            toMove.transform.position = canvas.transform.TransformPoint(position);
+        }
+
     }
 }
